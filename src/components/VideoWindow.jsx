8730d4868635788a0aeea18f1abefa05099dad20
mavroian <mavflorin@gmail.com>
2019-06-04 17:32:46 +0900

    refactored save poses

--- src/components/VideoWindow.jsx
+++ src/components/VideoWindow.jsx
@@ -1,51 +1,63 @@
 import React, { Component } from "react";
-import * as posenet from '@tensorflow-models/posenet';
+import * as posenet from "@tensorflow-models/posenet";
 
-export class VideoWindow extends Component{
-  render(){
+export class VideoWindow extends Component {
+  render() {
     const imageScaleFactor = 0.2;
     const outputStride = 16;
     let savePose = false;
     const recordedPoses = [];
     setInterval(() => {
       savePose = true;
-    }, 3000)
+    }, 3000);
+
+    function recordPose(pose) {
+      const correctPose = {};
+      for (let index = 0; index < pose.keypoints.length; index++) {
+        const part = pose.keypoints[index].part;
+        correctPose[part] = {};
+        correctPose[part].x = pose.keypoints[index].position.x;
+        correctPose[part].y = pose.keypoints[index].position.y;
+        correctPose[part].score = pose.keypoints[index].score;
+      }
+      recordedPoses.push(correctPose);
+    }
 
     async function bindPage() {
       const net = await posenet.load();
       let video;
-      try{
+      try {
         video = await loadVideo();
       } catch (e) {
-        alert (e);
+        alert(e);
         return;
       }
 
-      detectPoseInRealTime(video, net)
+      detectPoseInRealTime(video, net);
     }
 
     bindPage();
 
     async function setupCamera() {
       const video = document.getElementById("video");
-      console.log(video);
 
       if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        const stream = await navigator.mediaDevices.getUserMedia( {
-          "audio": false,
-          "video": true
-        })
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: false,
+          video: true
+        });
 
         video.srcObject = stream;
 
-        return new Promise( resolve => {
-          video.onloadedmetadata = () =>{
-            resolve (video);
-          }
+        return new Promise(resolve => {
+          video.onloadedmetadata = () => {
+            resolve(video);
+          };
         });
       } else {
-        const ErrorMessage = "This Browser Does Not Support Video Capture, Or This Device Does Not Have A Camera";
-        alert (ErrorMessage);
+        const ErrorMessage =
+          "This Browser Does Not Support Video Capture, Or This Device Does Not Have A Camera";
+        alert(ErrorMessage);
         return Promise.reject(ErrorMessage);
       }
     }
@@ -64,9 +76,13 @@ export class VideoWindow extends Component{
       const contentWidth = 800;
       const contentHeight = 600;
 
-
       async function poseDetectionFrame() {
-        const pose = await net.estimateSinglePose(video, imageScaleFactor, flipHorizontal, outputStride);;
+        const pose = await net.estimateSinglePose(
+          video,
+          imageScaleFactor,
+          flipHorizontal,
+          outputStride
+        );
 
         ctx.clearRect(0, 0, contentWidth, contentHeight);
         ctx.save();
@@ -75,16 +91,17 @@ export class VideoWindow extends Component{
         ctx.drawImage(video, 0, 0, contentWidth, contentHeight);
         ctx.restore();
 
-        if(savePose) {
-          recordedPoses.push(pose);
-          console.log(recordedPoses)
+        if (savePose) {
+          recordPose(pose);
+          console.log(recordedPoses);
           savePose = false;
         }
 
-
-        recordedPoses.forEach(pose => {
-          drawWristPoint(pose.keypoints[0], ctx)
-        })
+        for (const pose of recordedPoses) {
+          for (const part in pose) {
+            drawPoint(pose[part], ctx);
+          }
+        }
 
         requestAnimationFrame(poseDetectionFrame);
       }
@@ -92,18 +109,29 @@ export class VideoWindow extends Component{
       poseDetectionFrame();
     }
 
-    function drawWristPoint(keypoint, ctx) {
+    function drawPoint(keypoint, ctx) {
       ctx.beginPath();
-      ctx.arc(keypoint.position.x, keypoint.position.y, 20, 0, 2 * Math.PI)
+      ctx.arc(keypoint.x, keypoint.y, 5, 0, 2 * Math.PI);
       ctx.fillStyle = `rgb(255,255,0)`; // TODO not hardcode color
       ctx.fill();
     }
 
     return (
       <div>
-        <video id="video" width="800px" height="600px" autoPlay="1" style={{position: "absolute"}}></video>
-        <canvas id="canvas" width="800px" height="600px" style={{position: "absolute"}}></canvas>
+        <video
+          id="video"
+          width="800px"
+          height="600px"
+          autoPlay="1"
+          style={{ position: "absolute" }}
+        />
+        <canvas
+          id="canvas"
+          width="800px"
+          height="600px"
+          style={{ position: "absolute" }}
+        />
       </div>
-    )
+    );
   }
 }
