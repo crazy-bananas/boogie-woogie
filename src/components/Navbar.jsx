dd2bc65eb735ed80011f17ccb906aae8ba683698
Yasuhito Miyazaki and shruti <kizayami.yasu+shrutiitengg@gmail.com>
2019-06-04 11:38:36 +0900

    basic navbar done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Boogie Woogie</h1>
+      </div>
+    );
+  }
+}
