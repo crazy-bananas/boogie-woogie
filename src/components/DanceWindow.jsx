620c621da0d3ee8e20ce8c5048075aa22dfc9008
shruti and Johannes Jarbratt <shrutiitengg+johannes.jarbratt+github@gmail.com>
2019-06-05 18:07:13 +0900

    Started with react.ref

--- src/components/DanceWindow.jsx
+++ src/components/DanceWindow.jsx
@@ -6,12 +6,24 @@ import "../styles/counter.css";
 import { connect } from "react-redux";
 
 class DanceWindow extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.playButtonRef = React.createRef();
+  // }
+
+  // playAudio = () => {
+  //   this.playButtonRef.current.play();
+  // };
+  //
   render() {
     return (
       <div>
         {/* <VideoWindow /> */}
         <Counter style={{ position: "absolute" }} />
-        {this.props.isCountdownFinished && <AudioPlayer />}
+        {this.props.isCountdownFinished && (
+          <AudioPlayer ref={this.playButtonRef} />
+          // <AudioPlayer ref={this.playButtonRef} />
+        )}
       </div>
     );
   }
