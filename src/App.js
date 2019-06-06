61366e33517d46923cefa3c34ddbd32be2237c72
Johannes Jarbratt and Mavroian <johannes.jarbratt+github+mavflorin@gmail.com>
2019-06-06 15:29:40 +0900

    added score component

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         {!this.props.isSongSelected ? <SongMenu /> : <DanceWindow />}
+        <Score />
       </div>
     );
   }
