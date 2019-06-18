729883b6bc2a861f29daa8f9ddc0403d012a4d80
mavroian <mavflorin@gmail.com>
2019-06-18 21:51:43 +0900

    refactored profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,29 +1,15 @@
 import React, { Component } from "react";
 import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-// import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-// import FormControlLabel from "@material-ui/core/FormControlLabel";
-// import Checkbox from "@material-ui/core/Checkbox";
-// import Link from "@material-ui/core/Link";
-// import Grid from "@material-ui/core/Grid";
 import Fab from "@material-ui/core/Fab";
 import Box from "@material-ui/core/Box";
-//import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import bananaImage from "../images/bananaProfile.jpeg";
 import Card from "@material-ui/core/Card";
-//import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import "../styles/profile.css";
-//import { textAlign } from "@material-ui/system";
-import lifecycle from "react-pure-lifecycle";
-import axios from "axios";
 import Loading from "./Loading";
 import { connect } from "react-redux";
-import "../styles/profile.css";
 import backIcon from "../images/backArrow.png";
 import Navbar from "./Navbar";
 
@@ -34,20 +20,6 @@ class Profile extends Component {
     this.state = {
       data: 0
     };
-  }
-  // getUserScore = () => {
-  //   axios.get(``).then(data => {
-  //     //this.setState({});
-  //   });
-  // };
-  // getUserFriends = () => {
-  //   axios.get(``).then(data => {
-  //     //this.setState({});
-  //   });
-  // };
-  componentDidMount() {
-    //this.getUserScore();
-    //this.getUserFriends();
   }
   isUserDataFetched = () => {
     if (this.props.userAuthInfo) {
@@ -80,7 +52,7 @@ class Profile extends Component {
       <Card className="card">
         <CardContent className="cardBox">
           <Box className="friendBox">
-            <Avatar alt="Remy Sharp" src={bananaImage} className="avatar" />
+            <Avatar alt="Friend Picture" src={bananaImage} className="avatar" />
           </Box>
           <p>Jhonny Banana</p>
         </CardContent>
@@ -100,40 +72,7 @@ class Profile extends Component {
           <Box flexDirection="col">{this.isUserDataFetched()}</Box>
           <Card className="myCard">
             <CardContent className="cardBox">
-              <TextField
-                id="filled-name"
-                label="Song PLayed"
-                className="textField"
-                value="Different songs that the user has played"
-              />
-              <TextField
-                id="filled-name"
-                label="Rank"
-                className="textField"
-                value="Display ranking position"
-                margin="normal"
-              />
-              <TextField
-                id="filled-name"
-                label="Other data"
-                className="textField"
-                value=".................................."
-                margin="normal"
-              />
-              <TextField
-                id="filled-name"
-                label="Other data"
-                className="textField"
-                value=".................................."
-                margin="normal"
-              />
-              <TextField
-                id="filled-name"
-                label="Other Data"
-                className="textField"
-                value=".................................."
-                margin="normal"
-              />
+              
             </CardContent>
           </Card>
           <footer className="friends">
