d6862acdf1ff33877f95fa8c307eb3d1a493963f
mavroian <mavflorin@gmail.com>
2019-06-20 21:22:33 +0900

    Score table restyling

--- src/components/ScoreTable.jsx
+++ src/components/ScoreTable.jsx
@@ -7,11 +7,14 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import Avatar from "@material-ui/core/Avatar";
-import podium from "../images/podium.png"
+import podium from "../images/podiumTransparent.png";
+import Fab from "@material-ui/core/Fab";
+import star from "../images/star.png"
 
 const StyledTableCell = withStyles(theme => ({
   head: {
-    backgroundColor: theme.palette.common.white,
+    position:"sticky",
+    top:0,
     color: theme.palette.common.black
   },
   body: {
@@ -22,26 +25,28 @@ const StyledTableCell = withStyles(theme => ({
 const StyledTableRow = withStyles(theme => ({
   root: {
     "&:nth-of-type(odd)": {
-      backgroundImg: podium
+      backgroundImg: theme.palette.background.default
     }
   }
 }))(TableRow);
-
-
 
 const useStyles = makeStyles(theme => ({
   root: {
     marginTop: theme.spacing(2.5),
     overflowX: "auto",
-    height:500
+    height: 500,
+    backgroundImage: `url(${podium})`,
+    backgroundPositionY: 50,
+    backgroundRepeat:"no-repeat"
   },
   bigAvatar: {
     margin: 10,
     width: 35,
-    height: 35
+    height: 35,
+    zIndex: 2
   },
   table: {
-    width: 500,
+    width: 500
   }
 }));
 
@@ -52,10 +57,10 @@ export default function CustomizedTables(props) {
     <Paper className={classes.root}>
       <Table className={classes.table}>
         <TableHead >
-          <TableRow > 
-            <StyledTableCell  align="left"></StyledTableCell>
-            <StyledTableCell >Name</StyledTableCell>
-            <StyledTableCell >Score</StyledTableCell>
+          <TableRow>
+            <StyledTableCell style={{backgroundColor:"#E91E63", position:"sticky",top:0, zIndex: 2}} align="left"/>
+            <StyledTableCell style={{backgroundColor:"#E91E63", position:"sticky",top:0, zIndex: 2,fontWeight:"bold"}}>Name</StyledTableCell>
+            <StyledTableCell style={{backgroundColor:"#E91E63" , position:"sticky",top:0,  zIndex: 2,fontWeight:"bold"}}>Score</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -63,14 +68,31 @@ export default function CustomizedTables(props) {
             <StyledTableRow key={row.name}>
               <StyledTableCell align="left">
                 <Avatar
-                  style={{}}
+                  style={{ zIndex: 1}}
                   alt="Profile Picture"
                   src={row.pic}
                   className={classes.bigAvatar}
                 />
               </StyledTableCell>
               <StyledTableCell align="left">{row.user}</StyledTableCell>
-              <StyledTableCell align="left">{row.score}</StyledTableCell>
+              <StyledTableCell align="left">
+                <Fab
+                  size="medium"
+                  style={{
+                    zIndex:1,
+                    backgroundImage:`url(${star})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "contain",
+                    fontStyle: "inherit",
+                    backgroundColor: "transparent",
+                    boxShadow: "none"
+                  }}
+                  aria-label="Add"
+                  className="fab"
+                >
+                  {row.score}
+                </Fab>
+              </StyledTableCell>
             </StyledTableRow>
           ))}
         </TableBody>
