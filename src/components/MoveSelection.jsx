ec8f4a30314056384f3c9c66b75c86739517ed8c
Johannes Jarbratt <johannes.jarbratt+github@gmail.com>
2019-06-20 16:25:31 +0900

    Fixed Select Moves List

--- src/components/MoveSelection.jsx
+++ src/components/MoveSelection.jsx
@@ -12,11 +12,12 @@ import Grid from "@material-ui/core/Grid";
 import "../styles/moveselection.css";
 
 const MyList = styled(List)({
-  background: "rgba(218, 218, 218, 0.7)"
+  background: "rgba(218, 218, 218, 0.7)",
+  maxWidth: "50%",
+  minWidth: "25%"
 });
 
 const MyListItem = styled(ListItem)({
-  background: "rgba(242, 242, 242, 0.5);",
   width: "50vh"
 });
 
@@ -59,22 +60,31 @@ export class MoveSelection extends Component {
         <div id="move-selection">
           <h1>{this.state.title}</h1>
           <Grid container justify="center">
-            <MyList component="div">
-              {this.state.moves.length !== 0 &&
-                this.state.moves.map((move, index) => {
-                  return (
-                    <MyListItem 
-                      onClick={e => this.props.setSelectedMoveId(e)}
+            <MyList>
+            {this.state.moves.length !== 0 &&
+              this.state.moves.map((move, index) => {
+                return (
+                  <ListItem 
+                    style ={{display:"inline-flex"}}
+                    key={index}
+                    button
+                  >
+                    <ListItemIcon>
+                      <MusicVideo />
+                    </ListItemIcon>
+                    <MyListItem
+                      style={{ margin: 0, padding: 0 }}
                       key={index}
                       data-key={move._id}
+                      data-index={index}
+                      onClick={e => this.props.setSelectedMoveId(e)}
                     >
-                      <ListItemIcon>
-                        <MusicVideo />
-                      </ListItemIcon>
                       {move.name}
                     </MyListItem>
-                  );
-                })}
+                  </ListItem>
+                );
+                })
+              }
             </MyList>
           </Grid>
         </div>
